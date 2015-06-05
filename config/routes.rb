174cf650d41777063fdf2dde86b90672b0078bce
blessings <blessings.mhango@gmail.com>
2015-06-05 15:34:48 +0200

    added mailer

--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,12 @@ Rails.application.routes.draw do
   resources :sessions, only: [:new, :create, :destroy]
   get '/log_in', to: 'sessions#new', as: :log_in
   delete '/log_out', to: 'sessions#destroy', as: :log_out
+  
+  resources :users, only: [:new, :create] do
+		member do
+		  get :activate
+		end
+  end
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
