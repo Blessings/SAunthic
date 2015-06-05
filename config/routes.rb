2306992eee7e863e4ebc61b97788ecf811444846
blessings <blessings.mhango@gmail.com>
2015-06-05 15:12:10 +0200

    before remember me

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,14 @@
 Rails.application.routes.draw do
-  
-
+ 
   get '/secret', to: 'pages#index', as: :secret
   root to: 'pages#index'
   
   resources :users, only: [:new, :create]
   get '/sign_up', to: 'users#new', as: :sign_up
+  
+  resources :sessions, only: [:new, :create, :destroy]
+  get '/log_in', to: 'sessions#new', as: :log_in
+  delete '/log_out', to: 'sessions#destroy', as: :log_out
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
